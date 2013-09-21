2b6bf49f4e79f2e97c080d50807d6278c2eb4f58
Noah Peart <noah.v.peart@gmail.com>
2013-09-21 14:12:26 -0400

    knapsack

---
+++ pythoncodes/knapsack.py
@@ -0,0 +1,10 @@
+## Knapsack problem
+## Input: n items - each has a value:
+## - value v (nonnegative)
+## - size w (nonnegative and integral)
+## - capacity W (nonnegative integer)
+
+## Output:  a subset S of the initial group that maximizes the sum of vs in S
+##  subject to the constraint that the sum of the sizes (ws) is less than or equal to 
+##  the capacity W
+
