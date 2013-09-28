4c367c6f5d7b2bf96517636c7b6519e6de8c6e98
Noah Peart <noah.v.peart@gmail.com>
2013-09-28 11:31:04 -0400

    knap

--- pythoncodes/knapsack.py
+++ pythoncodes/knapsack.py
@@ -8,21 +8,13 @@
 ##  subject to the constraint that the sum of the sizes (ws) is less than or equal to 
 ##  the capacity W
 import sys
-import collections
-import functools
-
-
-lines = [line.strip() for line in open('C:/users/noah/desktop/algorithms2/knapsack1.txt')]
-maxweight = int(lines[0][0])
-
-items = [[int(num) for num in line.split()] for line in lines[1:]]
-
 
 ## small problem
 lines = [line.strip() for line in open('c:/home/algorithms2/knapsack1.txt')]
 maxweight = int(lines[0].split()[0])
 items = int(lines[0].split()[1])
 lines = [[int(num) for num in line.split()] for line in lines[1:]]
+items = [(line[0], line[1]) for line in lines]
 
 ## big problem
 lines = [line.strip() for line in open('c:/home/algorithms2/knapsack_big.txt')]
@@ -32,72 +24,49 @@ lines = [[int(num) for num in line.split()] for line in lines[1:]]
 # lines as a tuple
 items = [(line[0], line[1]) for line in lines]
 
-def knapsack(items, maxweight):
-    """
-    Solve the knapsack problem by finding the most valuable
-    subsequence of `items` subject that weighs no more than
-    `maxweight`.
-    `items` is a sequence of pairs `(value, weight)`, where `value` is
-    a number and `weight` is a non-negative integer.
-    `maxweight` is a non-negative integer.
-    Return a pair whose first element is the sum of values in the most
-    valuable subsequence, and whose second element is the subsequence.
-    >>> items = [(4, 12), (2, 1), (6, 4), (1, 1), (2, 2)]
-    >>> knapsack(items, 15)
-    (11, [(2, 1), (6, 4), (1, 1), (2, 2)])
-    """
-    # Return the value of the most valuable subsequence of the first i
-    # elements in items whose weights sum to no more than j.
-    def bestvalue(i, j):
-        if i == 0: return 0
-        value, weight = items[i - 1]
-        if weight > j:
-            return bestvalue(i - 1, j)
-        else:
-            return max(bestvalue(i - 1, j),
-                       bestvalue(i - 1, j - weight) + value)
-    return bestvalue(len(items), maxweight)
-
-## Recursive version to handle larger memory case
+## Recursive version to handle larger memory case, will store each recursive call in 
+##  python dictionary for lookup later
 def recursiveKnapsack(items, maxweight):
-    @memoized
+    P = {}
     def loop(numItems, lim):
-        if numItems == 0:
-            return 0
-        elif items[numItems-1][1] > lim:
-            return loop(numItems-1,lim)
-        else:
-            return max(loop(numItems-1,lim), loop(numItems - 1, lim - items[numItems-1][1]) + items[numItems-1][0])
+        if (numItems, lim) not in P:
+            if numItems == 0:
+                P[(numItems, lim)] = 0
+            elif items[numItems-1][1] > lim:
+                P[(numItems, lim)] = loop(numItems-1,lim)
+            else:
+                P[(numItems, lim)] = max(loop(numItems-1,lim), loop(numItems - 1, lim - items[numItems-1][1]) + items[numItems-1][0])
+        return P[(numItems, lim)]
     return loop(len(items), maxweight)
 
 sys.setrecursionlimit(6000)
-ans = recursiveKnapsack(lines, maxweight)
+ans = recursiveKnapsack(items, maxweight)
 
-sys.setrecursionlimit(8000)
-ans = knapsack(items, maxweight)
+## store values in 2-D array, takes too much memory for larger cases
+def knapsack(items, maxweight):
+    bestvalues = [[0] * (maxweight + 1) for i in range(len(items) + 1)]
+    # Enumerate through the items and fill in the best-value table
+    for i, (value, weight) in enumerate(items):
+        # row 0 is full of 0s
+        i += 1
+        for capacity in range(maxweight + 1):
+            # Edge case: weight bigger than sub-capacity
+            if weight > capacity:
+                bestvalues[i][capacity] = bestvalues[i - 1][capacity]
+            else: 
+                # Choose between case where i is in and i is out of optimal soln
+                case1 = bestvalues[i - 1][capacity]
+                case2 = bestvalues[i - 1][capacity - weight] + value
+                bestvalues[i][capacity] = max(case1, case2)
+    # Reconstruction
+    reconstruction = []
+    i = len(items)
+    j = maxweight
+    while i > 0:
+        if bestvalues[i][j] != bestvalues[i - 1][j]:
+            reconstruction.append(items[i - 1])
+            j -= items[i - 1][1]
+        i -= 1
+    reconstruction.reverse()
+    return bestvalues[len(items)][maxweight], reconstruction
 
-class memoized(object):
-   '''Decorator. Caches a function's return value each time it is called.
-   If called later with the same arguments, the cached value is returned
-   (not reevaluated).
-   '''
-   def __init__(self, func):
-      self.func = func
-      self.cache = {}
-   def __call__(self, *args):
-      if not isinstance(args, collections.Hashable):
-         # uncacheable. a list, for instance.
-         # better to not cache than blow up.
-         return self.func(*args)
-      if args in self.cache:
-         return self.cache[args]
-      else:
-         value = self.func(*args)
-         self.cache[args] = value
-         return value
-   def __repr__(self):
-      '''Return the function's docstring.'''
-      return self.func.__doc__
-   def __get__(self, obj, objtype):
-      '''Support instance methods.'''
-      return functools.partial(self.__call__, obj)
