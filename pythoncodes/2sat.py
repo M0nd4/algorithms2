28702b8b86d3b304729c0bb795f1e9af94d39c6b
Noah Peart <noah.v.peart@gmail.com>
2013-10-13 17:06:31 -0400

    2sat with reduction

--- pythoncodes/2sat.py
+++ pythoncodes/2sat.py
@@ -23,45 +23,42 @@ def readSat (filename):
     lines = [tuple([int(num) for num in line.split()]) for line in lines[1:]]
     return numVars, lines
 
-def twoSat(n, clauses):
+def twoSat(clauses, vs):
     "Runs 2sat to determine if sequence of clauses is satisfiable."
-    i = math.floor(math.log(n, 2)) + 1
+    i = math.floor(math.log(len(vs), 2)) + 1
     while i >= 0:
-        guess = randGuess(n)
-        j = 2*n^2
+        guess = randGuess(vs)
+        j = 2*len(vs)^2
         while j >= 0:
             if isSatisfiable(guess, clauses):
                 return 1
             else: # Pick arbitrary unsatisfied clauses and flip value of one variable
                 flipClause(guess, clauses)
             j -= 1
-        print(i)
         i -= 1
     return 0
 
 def flipClause(guess, clauses):
     "Pick arbitrary unsatisfied clause"
     for i in clauses:
-        if guess[abs(i[0])-1] != i[0] and guess[abs(i[1])-1] != i[1]:
+        if guess[abs(i[0])] != i[0] and guess[abs(i[1])] != i[1]:
             c = choice([1,0])
-            guess[abs(i[c])-1] = -1 * guess[abs(i[c])-1]
+            guess[abs(i[c])] = -1 * guess[abs(i[c])]
 
 def isSatisfiable(guess, clauses):
     "Test to see if guess satisfies all clauses"
     for i in clauses:
-        if guess[abs(i[0])-1] != i[0] and guess[abs(i[1])-1] != i[1]:
+        if guess[abs(i[0])] != i[0] and guess[abs(i[1])] != i[1]:
             return False
     return True
     
-def randGuess(n):
+def randGuess(vs):
     "Intialize a random guess at satisfying conditions"
-    n = list(range(1,n+1))
+    n = [v for v in vs]
     sign = (-1,1)
-    cl = [choice(sign) * num for num in n]
+    cl = {num: choice(sign)*num for num in n}
     return cl
     
-# filename = '2sat1.txt'
-# n, clauses = readSat(filename)
 # Run unit test
 filestub = "C:/home/algorithms2/2sat"
 def runTest(filestub, numFiles):
@@ -71,7 +68,54 @@ def runTest(filestub, numFiles):
         sat_file = filestub + str(i) + '.txt'
         print("sat"+str(i)+":\n")
         n, clauses = readSat(sat_file)
-        ans = twoSat(n, clauses)
+        c,v = sat2dict(n, clauses)
+        c,v = reduce(c,v)
+        ans = twoSat(c,v)
         print("Result: " + str(ans) +"\n")
-        output + [ans]
+        output += [ans]
     return output
+
+####################################################################################
+##  Pre-processing
+def removeFixed(n, clauses):
+    "Remove fixed clauses from input.  Fixed clauses are those that contain a variable that is the same sign throughout all the input clauses."
+    for i in list(range(1,n)):
+        fixed = [j[0] for j in clauses if abs(j[0]) == i] + [i[1] for i in clauses if abs(i[1]) == i]
+        if all(t > 0 for t in fixed) or all(t < 0 for t in fixed) or fixed == []:
+            clauses = [j for j in clauses if abs(j[0]) != i and abs(j[1]) != i]
+    return len(clauses), clauses
+
+def sat2dict(n, clauses):
+    "Create dictionaries with clauses and variables"
+    clauseDict = {cl:1 for cl in clauses}
+    varDict = {i: [] for i in list(range(1,n+1))}
+    for i in clauseDict:
+        varDict[abs(i[0])] += [i]
+        varDict[abs(i[1])] += [i]
+    return clauseDict, varDict
+
+def reduceDict(cDict, vDict):
+    "Remove fixed clauses from variable and clause dictionaries"
+    for var in vDict:
+        if vDict[var] == []:
+            continue
+        fixed = [j[0] for j in vDict[var] if abs(j[0]) ==var] + [i[1] for i in vDict[var] if abs(i[1]) == var]
+        if all(t > 0 for t in fixed) or all(t < 0 for t in fixed):
+            for cl in vDict[var]:
+                cDict[cl] = 0
+                for v in cl:
+                    if abs(v) != var:
+                        if cl in vDict[abs(v)]:
+                            vDict[abs(v)].remove(cl)
+            vDict[var] = []
+    cD = {cl: l for cl, l in cDict.items() if l != 0}
+    vD = {v: l for v, l in vDict.items() if l != []}
+    return cD, vD
+
+def reduce(cDict, vDict):
+    "Reduce until no change"
+    cD, vD = reduceDict(cDict, vDict)
+    if cD == cDict:
+        return cD, vD
+    else:
+        return reduce(cD, vD)
